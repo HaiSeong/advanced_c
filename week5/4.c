e65bb2fac1b836890ad66085a4578fbde9c09e40
Haiseong Jeong <hajeong@c8r4s13.42seoul.kr>
2022-10-03 22:02:27 +0900

    4 : 50

--- week5/4.c
+++ week5/4.c
@@ -4,28 +4,44 @@
 
 typedef struct student
 {
-    char name[10];
-    int score;
+    char name[20];
+    int score1;
+    int score2;
+    int score3;
 } student;
 
 int main()
 {
     student S[5];
-    char temp_name[10];
-    int temp_score;
+    char temp_name[20];
+    int temp_score1, temp_score2,temp_score3, N;
     double avg, sum = 0;
 
-    for (int i=0; i < 5; i++)
+    scanf("%d", &N);
+
+    for (int i=0; i < N; i++)
     {
-        scanf("%s%d", temp_name, &temp_score);
+        sum = 0;
+        avg = 0;
+        scanf("%s%d%d%d", temp_name, &temp_score1, &temp_score2, &temp_score3);
         strcpy(S[i].name, temp_name);
-        S[i].score = temp_score;
-        sum += S[i].score;
-    }
-    avg = sum / 5.0;
-     for (int i=0; i < 5; i++)
-    {
-        if (S[i].score <= avg)
-            printf("%s\n", S[i].name);
+        S[i].score1 = temp_score1;
+        S[i].score2 = temp_score2;
+        S[i].score3 = temp_score3;
+        sum = S[i].score1 + S[i].score2 + S[i].score3;
+        avg = sum / 3.0;
+
+        printf("%s %.1lf", S[i].name, avg);
+
+        if (avg > 90)
+            printf(" A");
+        else if (avg > 80)
+            printf(" B");
+        else if (avg > 70)
+            printf(" C");
+        else
+            printf(" F");
+
+        printf("\n");
     }
 }
